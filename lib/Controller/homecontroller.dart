c25fadc93d7766a390e69df82dc88c9c725c7542
Nabeh119 <nabeh119y@gmail.com>
2026-03-31 12:08:55 +0300

    update project

---
+++ lib/Controller/homecontroller.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class HomeGetBuilderController
+    extends GetxController {
+  int counter = 0;
+
+  void increment() {
+    counter++;
+    update();
+  }
+
+  void decrement() {
+    counter--;
+    update();
+  }
+}
+
+class HomeGetXController
+    extends GetxController {
+  RxInt counter = 0.obs;
+
+  void increment() {
+    counter++;
+  }
+
+  void decrement() {
+    counter--;
+  }
+}
---
+++ lib/View/home.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/state_manager.dart';
+import 'package:getx_app/Controller/homecontroller.dart';
+
+class HomePage extends StatelessWidget {
+  HomeGetXController controller =
+      Get.put(HomeGetXController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text("GetBuilderPage"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center,
+          children: [
+            //GetBuilder Controller
+            GetBuilder<
+              HomeGetBuilderController
+            >(
+              init:
+                  HomeGetBuilderController(),
+              builder: (controller) {
+                return Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment
+                          .center,
+                  children: [
+                    Text(
+                      "GetBuilder Controller",
+                      style: TextStyle(
+                        fontWeight:
+                            FontWeight
+                                .bold,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.add,
+                      ),
+                      onPressed: () {
+                        controller
+                            .increment();
+                      },
+                    ),
+                    Text(
+                      "${controller.counter}",
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.remove,
+                      ),
+                      onPressed: () {
+                        controller
+                            .decrement();
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+            //GetX Controller
+            GetX<HomeGetXController>(
+              init:
+                  HomeGetXController(),
+              builder: (controller) {
+                return Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment
+                          .center,
+                  children: [
+                    Text(
+                      "GetX Controller",
+                      style: TextStyle(
+                        fontWeight:
+                            FontWeight
+                                .bold,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.add,
+                      ),
+                      onPressed: () {
+                        controller
+                            .increment();
+                      },
+                    ),
+                    Text(
+                      "${controller.counter.value}", //obx Int we need to add counter.value
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.remove,
+                      ),
+                      onPressed: () {
+                        controller
+                            .decrement();
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+
+            //OBX
+            //HomeGetXController controller = Get.put(HomeGetXController());
+            //in Obx we can have Mlti Controller
+            Obx(() {
+              return Row(
+                mainAxisAlignment:
+                    MainAxisAlignment
+                        .center,
+                children: [
+                  Text(
+                    "OBX Controller",
+                    style: TextStyle(
+                      fontWeight:
+                          FontWeight
+                              .bold,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.add,
+                    ),
+                    onPressed: () {
+                      controller
+                          .increment();
+                    },
+                  ),
+                  Text(
+                    "${controller.counter.value}", //obx Int we need to add counter.value
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.remove,
+                    ),
+                    onPressed: () {
+                      controller
+                          .decrement();
+                    },
+                  ),
+                ],
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
